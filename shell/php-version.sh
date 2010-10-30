22facadee4e9c775133f021f9b374a7677793db6
Gerard van Helden <drm@melp.nl>
2010-10-30 05:34:34 -0700

    some tweaks with --off and --force, and introduced PHP_PREFIX and BIN_PREFIX variables

--- shell/php-version.sh
+++ shell/php-version.sh
@@ -5,10 +5,8 @@
 # Utility script to switch php versions installed in
 # /usr/local/php/$versionNumber/
 
-# Available binaries are symlinked in /usr/local/bin/
-# Current (running) version is written to /var/run/php-version.current
-
-
+[ "$PHP_PREFIX" == "" ] && PHP_PREFIX="/usr/local/php"
+[ "$BIN_PREFIX" == "" ] && BIN_PREFIX="/usr/local/bin"
 
 function check_prefix {
 	prefix=$1
@@ -20,41 +18,47 @@ function check_prefix {
 
 
 function disable {
-	prefix="/usr/local/php/$1"
+	prefix="$PHP_PREFIX/$1"
 	check_prefix $prefix
 	for i in $prefix/bin/*; do
-		if [ -h "/usr/local/bin/`basename $i`" ]; then
-			( cd /usr/local/bin && rm -f `basename $i` )
+		if [ -h "$BIN_PREFIX/`basename $i`" ]; then
+			( cd $BIN_PREFIX && rm -f `basename $i` )
 		fi
 	done
 }
 
 
 function enable {
-	prefix="/usr/local/php/$1"
+	prefix="$PHP_PREFIX/$1"
 	check_prefix $prefix
 	for i in $prefix/bin/*; do
 		if [ ! -h $i ]; then
-			( cd /usr/local/bin/ \
+			( cd $BIN_PREFIX \
 				&& ln -s $i );
 		fi
 	done
 }
 
+if [ "`which php`" != "" -a -x "`which php`" ]; then
+	current=`ls -l $(which php) | sed s~.*$PHP_PREFIX/~~ | egrep -o '[^/]+' | head -1`
+fi
+
+
 if [ "" == "$1" ]; then
 	echo "Usage: $0 {version [--force]|--off}" >&2
+    if [ "$current" != "" ]; then
+        echo "Current version: $current";
+    fi;
 	exit -1
 fi;
 
-if [ "`which php`" != "" -a -x "`which php`" ]; then
-	current=`ls -l $(which php) | egrep -o '/[0-9.]+/' | tr -d /`
-elif [ "$2" != "--force" ]; then
+if [ "$current" == "" ] &&  [ "$2" != "--force" ]; then
 	echo "php is not available, can not check current version"
 	echo "Use --force option to force switch"
 	exit -2;
 fi
 
-if [ "$current" == "$1" ]; then
+if [ "$current" == "$1" ] && [ "$2" != "--force" ]; then
 	echo "$current already available"
 	exit 0;
 elif [ "" != "$current" ]; then
